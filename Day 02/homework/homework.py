80bf365c2c6c7e3def001305d26b6e87f4eee3fa
kakhadzeluka12-afk <kakhadzeluka12@gmail.com>
2025-10-03 11:25:59 +0400

    first commit

---
+++ Day 02/homework/homework.py
@@ -0,0 +1,36 @@
+# მთელი რიცხვი (integer)
+age = 15
+print(age)
+
+# ათწილადი რიცხვი (float)
+height = 1.72
+print(height)
+
+# ტექსტი (string)
+name = "Luka"
+print(name)
+
+# სია/list
+fruits =["apple","watermelon","banana","strawberry"]
+
+person={"name": "Luka", age:15 }
+
+print(person)
+
+my_number = 17 , 18 ,19 ,20 ,21
+print(my_number)
+
+my_number = 21 , 22 , 27 , 29 ,101 ,821 ,932
+print(my_number)
+
+Name="luka"
+print(name+3)
+
+#ეს მოცემული კოდი გამოიტანს error რადგან 
+#სტრინგს (text) და მთელ რიცხვს (integer) ვერ ვუმატებთ ერთმანეთზე პირდაპირ
+
+first_name="Luka"
+Last_name="Kakhadze"
+full_name="luka kakhadze"
+
+print(full_name)
