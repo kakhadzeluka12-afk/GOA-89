13c88c96474f4763d0929ea41b13f40a804b6508
kakhadzeluka12-afk <kakhadzeluka12@gmail.com>
2025-10-05 15:37:07 +0400

    commit

---
+++ Day 03/classwork/classwork.py
@@ -0,0 +1,7 @@
+product1 = input("banana")
+product2 = input("strawberry")
+product3 = input("watermelon")
+
+print(product1)
+print(product2)
+print(product3)
---
+++ Day 03/homework/homework.py
@@ -0,0 +1,36 @@
+name = input ("luka")
+surname = input("kakhadze")
+age = input("15")
+height = input("1.74")
+
+print("luka", name)
+print("kakhadze:", surname)
+print("15", age)
+print("1.74", height)
+
+
+number_str = "17"
+number_int = int(number_str)
+print(number_int + 5) #22
+
+number_str = "17.7"
+number_float = float(number_str)
+print(number_float + 2) #19.7
+
+num1 = float(input("17"))
+num2 = float(input("1 "))
+num3 = float(input("2"))
+
+
+average = (num1 + num2 + num3) / 3
+print(average)
+print(num1)
+print(num2)
+print(num3)
+
+num1 = float(input( "17"))
+num2 = float(input("3"))
+operator = input("+")
+
+result=num1+num2
+print(result)
