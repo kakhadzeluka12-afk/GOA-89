df3b8e99ca65ecf277654ec3331eb5a7dfb8f4fa
kakhadzeluka12-afk <kakhadzeluka12@gmail.com>
2025-10-11 17:26:56 +0400

    commit

---
+++ Day 06/classwork/classwork.py
@@ -0,0 +1,35 @@
+for i in range(0, 11):  # 0-დან 10-ის ჩათვლით
+    if i % 2 == 0:
+        print(f"{i} ლუწია")
+    else:
+        print(f"{i} კენტია")
+
+# for loop არის კოდის გამმეორება მაგალითდ როცა გვინდა
+#  რომ რაიმე სახელი ან რაიმე სიტყვა დავწეროთ ბევრჯერ ეს კეთდება  
+# for loop გამოყენებით
+
+for i in range(10):
+    print("ლუკა")
+
+
+name = "Luka"
+for letter in name:
+    print(letter)
+
+
+total = 0
+
+for i in range(5):
+    print(str)( + 1) + ("17")
+    
+    number = int(input(8)) 
+
+    total = number
+
+    total = 25
+
+for i in range(0, 11): 
+    if i % 2 == 0:
+        print(f"{i} ლუწია")
+    else:
+        print(f"{i} კენტია")
---
+++ Day 06/homework/homework.py
@@ -0,0 +1,59 @@
+#კვადრატების ჯამი 
+sum_of_squares= 0
+for i in range(1 , 11):
+    sum_of_squares =+ i ** 2
+    print (sum_of_squares)
+
+#შეამოწმეთ იყოფა თუ არა 3ზე
+
+total_count = 0
+for i in range (1,100):
+    1 , 100  % 3
+    print(total_count)
+
+#sum_of_odds
+
+sum_of_odds = 0
+for i in range (1,101):
+  if i % 2  == 1:
+     sum_of_odds += i
+     print(sum_of_odds)
+
+
+# შემოატანინეთ მომხმარებელს რიცხვი
+number = int(input("შეიყვანეთ რიცხვი: "))
+for i in range(1 ,number + 1):
+      print(i)
+
+
+# დაბეჭდებ რიცხვი იმდენჯერ რამდენი ციფრიც შემოიტანა მომხმარებელმა
+
+number = int(input("შეიყვანეთ რიცხვი: "))
+
+for i in range(number):
+    print("Hello")
+
+# ახსენით რა არის while loop  და for loop კომენტარის სახით:
+
+# while loop არის რომელიც მუშობს სანამ ეკრანზე მოცემული პირობა სიმართლეა ანუ true 
+
+# for loop არის ციკლი რომელიც გამოიყენება ტეკსტის  რამდენიმეჯერ დასაბეჭდათ 
+
+
+
+# while loop გამოყენებით დაბეჭდეთ რიცხვი 1-10
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# 10)
+um = int(input("შეიყვანეთ ნებისმიერი რიცხვი: "))
+
+if num < 0:
+    print("რიცხვი უარყოფითია")
+elif num > 0:
+    print("რიცხვი დადებითია")
+else:
+    print("რიცხვი ნულია")
