d86c27cad3a82f9832af50b9964fbf66f0313809
kakhadzeluka12-afk <kakhadzeluka12@gmail.com>
2025-10-18 20:20:11 +0400

    commit

---
+++ Day 10/homework/homework.py
@@ -0,0 +1,22 @@
+my_list =[10,20,30,40,50,60]
+print(my_list[2:5])
+
+
+my_list = [1,2,3,4,5]
+print(my_list[:3])
+
+my_list [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(my_list[::2])
+
+
+my_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+my_list[:3] = ['x', 'y', 'z']
+print(my_list)
+
+
+text = "GOA"
+num = int(input("შეიყვანეთ რიცხვი: "))
+print(text[:num+1])
+
+my_list = [100, 200, 300, 400, 500, 600]
+print(my_list[-3:]) 
